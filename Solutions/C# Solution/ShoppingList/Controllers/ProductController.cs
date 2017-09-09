59af2a221f09c065088bbe9813d98c946aa7b445
Kaloyan-Stankov <kalo944@gmail.com>
2017-09-09 12:45:07 +0300

    uploading

---
+++ Solutions/C# Solution/ShoppingList/Controllers/ProductController.cs
@@ -0,0 +1,92 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using ShoppingList.Models;
+
+namespace ShoppingList.Controllers
+{
+    [ValidateInput(false)]
+    public class ProductController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public ActionResult Index()
+        {
+            using (var db = new ShoppingListDbContext())
+            {
+                var products = db.Products.ToList();
+
+                return View(products);
+            }
+        }
+
+        [HttpGet]
+        [Route("create")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("create")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var db = new ShoppingListDbContext())
+                {
+                    db.Products.Add(product);
+                    db.SaveChanges();
+                    return Redirect("/");
+                }
+            }
+            return View("Create");
+        }
+
+        [HttpGet]
+        [Route("edit/{id}")]
+        public ActionResult Edit(int? id)
+        {
+            using (var db = new ShoppingListDbContext())
+            {
+                var product = db.Products.Find(id);
+
+                if (product == null)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                return View(product);
+            }
+        }
+
+        [HttpPost]
+        [Route("edit/{id}")]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditConfirm(int? id, Product productModel)
+        {
+            using (var db = new ShoppingListDbContext())
+            {
+                var product = db.Products.Find(id);
+
+                if (product == null)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                if (ModelState.IsValid)
+                {
+                    product.Name = productModel.Name;
+                    product.Priority = productModel.Priority;
+                    product.Quantity = productModel.Quantity;
+                    product.Status = productModel.Status;
+
+                    db.SaveChanges();
+                }
+                
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
